68b77b32b124b9cc3700bc3326e00aa71deda0cc
Bryan <bdegante0906@gmail.com>
2026-09-02 16:34:11 -0500

    made top Sellers dynamic, and added a skeleton state

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,47 +1,101 @@
-import React, { useEffect } from "react";
-import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const TopSellers = () => {
-  useEffect(() => {
-    
-  })
-  return (
-    <section id="section-popular" className="pb-5">
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-12">
-            <div className="text-center">
-              <h2>Top Sellers</h2>
-              <div className="small-border bg-color-2"></div>
-            </div>
-          </div>
-          <div className="col-md-12">
-            <ol className="author_list">
-              {new Array(12).fill(0).map((_, index) => (
-                <li key={index}>
-                  <div className="author_list_pp">
-                    <Link to="/author">
-                      <img
-                        className="lazy pp-author"
-                        src={AuthorImage}
-                        alt=""
-                      />
-                      <i className="fa fa-check"></i>
-                    </Link>
-                  </div>
-                  <div className="author_list_info">
-                    <Link to="/author">Monica Lucas</Link>
-                    <span>2.1 ETH</span>
-                  </div>
-                </li>
-              ))}
-            </ol>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+	const [sellers, setSellers] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+	useEffect(() => {
+		async function getSellers() {
+			const { data } = await axios.get(
+				'https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers',
+			);
+
+			setSellers(data);
+			setIsLoading(false);
+		}
+		getSellers();
+	}, []);
+	return (
+		<section id="section-popular" className="pb-5">
+			<div className="container">
+				<div className="row">
+					<div className="col-lg-12">
+						<div className="text-center">
+							<h2>Top Sellers</h2>
+							<div className="small-border bg-color-2"></div>
+						</div>
+					</div>
+					<div className="col-md-12">
+						<ol className="author_list">
+							{isLoading
+								? [...Array(12)].map((_, index) => (
+										<li key={index}>
+											<div className="author_list_pp">
+												<Link to="/">
+													<div
+														className="skeleton-box"
+														style={{
+															width: '50px',
+															height: '50px',
+															borderRadius: '50%',
+														}}
+													/>
+													<i className="fa fa-check"></i>
+												</Link>
+											</div>
+											<div className="author_list_info">
+												<Link to="/">
+													<div
+														className="skeleton-box"
+														style={{
+															width: '100px',
+															height: '20px',
+														}}
+													/>
+												</Link>
+												<span>
+													<div
+														className="skeleton-box"
+														style={{
+															width: '40px',
+															height: '20px',
+														}}
+													/>
+												</span>
+											</div>
+										</li>
+									))
+								: sellers.map((seller, index) => (
+										<li key={index}>
+											<div className="author_list_pp">
+												<Link
+													to={`/author/${seller.authorId}`}
+												>
+													<img
+														className="lazy pp-author"
+														src={seller.authorImage}
+														alt=""
+													/>
+													<i className="fa fa-check"></i>
+												</Link>
+											</div>
+											<div className="author_list_info">
+												<Link
+													to={`/author/${seller.authorId}`}
+												>
+													{seller.authorName}
+												</Link>
+												<span>{seller.price} ETH</span>
+											</div>
+										</li>
+									))}
+						</ol>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default TopSellers;
