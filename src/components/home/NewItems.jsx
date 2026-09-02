a7ac6380b952491e1cf694565eb83bcc830ad842
Bryan <bdegante0906@gmail.com>
2026-09-02 15:05:41 -0500

    skeleton loading state was added, Newitems is now finished

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -9,7 +9,6 @@ import CountDown from '../UI/CountDown';
 const NewItems = () => {
 	const [isLoading, setIsLoading] = useState(true);
 	const [newItemsData, setNewItemsData] = useState([]);
-	
 
 	useEffect(() => {
 		async function getItems() {
@@ -27,8 +26,8 @@ const NewItems = () => {
 
 	const options = {
 		nav: true,
-    items: 4,
-    loop: true,
+		items: 4,
+		loop: true,
 		margin: 10,
 		dots: false,
 		responsive: {
@@ -57,95 +56,202 @@ const NewItems = () => {
 							<div className="small-border bg-color-2"></div>
 						</div>
 					</div>
+					{isLoading ? (
+						<OwlCarousel
+							className="owl-theme"
+							{...options}
+							key={isLoading ? 'loading' : 'loaded'}
+						>
+							{[...Array(6)].map((_, index) => (
+								<div key={index}>
+									<div className="nft__item">
+										<div className="author_list_pp">
+											<Link
+												to="/"
+												data-bs-toggle="tooltip"
+												data-bs-placement="top"
+												title="Creator: Monica Lucas"
+											>
+												<div
+													className="skeleton-box"
+													style={{
+														width: '50px',
+														height: '50px',
+														borderRadius: '50%',
+													}}
+												/>
+												<i className="fa fa-check"></i>
+											</Link>
+										</div>
+										<div className="nft__item_wrap">
+											<div className="nft__item_extra">
+												<div className="nft__item_buttons">
+													<button>Buy Now</button>
 
-					<OwlCarousel className="owl-theme" {...options}>
-						{newItemsData.map((item, index) => (
-							<div key={index}>
-								<div className="nft__item">
-									<div className="author_list_pp">
-										<Link
-											to={`/author/${item.authorId}`}
-											data-bs-toggle="tooltip"
-											data-bs-placement="top"
-											title="Creator: Monica Lucas"
-										>
-											<img
-												className="lazy"
-												src={item.authorImage}
-												alt=""
-											/>
-											<i className="fa fa-check"></i>
-										</Link>
-									</div>
+													<div className="nft__item_share">
+														<h4>Share</h4>
 
-									{item.expiryDate && (
-										<CountDown
-											expireTime={item.expiryDate}
-										/>
-									)}
+														<a
+															href=""
+															target="_blank"
+															rel="noreferrer"
+														>
+															<i className="fa fa-facebook fa-lg"></i>
+														</a>
 
-									<div className="nft__item_wrap">
-										<div className="nft__item_extra">
-											<div className="nft__item_buttons">
-												<button>Buy Now</button>
+														<a
+															href=""
+															target="_blank"
+															rel="noreferrer"
+														>
+															<i className="fa fa-twitter fa-lg"></i>
+														</a>
 
-												<div className="nft__item_share">
-													<h4>Share</h4>
-
-													<a
-														href=""
-														target="_blank"
-														rel="noreferrer"
-													>
-														<i className="fa fa-facebook fa-lg"></i>
-													</a>
-
-													<a
-														href=""
-														target="_blank"
-														rel="noreferrer"
-													>
-														<i className="fa fa-twitter fa-lg"></i>
-													</a>
-
-													<a href="">
-														<i className="fa fa-envelope fa-lg"></i>
-													</a>
+														<a href="">
+															<i className="fa fa-envelope fa-lg"></i>
+														</a>
+													</div>
 												</div>
 											</div>
+
+											<Link to="/">
+												<div
+													className="skeleton-box"
+													style={{
+														width: '100%',
+														height: '350px',
+													}}
+												/>
+											</Link>
 										</div>
 
-										<Link
-											to={`/item-details/${item.nftId}`}
-										>
-											<img
-												src={item.nftImage}
-												className="lazy nft__item_preview"
-												alt=""
+										<div className="nft__item_info">
+											<Link to="/">
+												<div
+													className="skeleton-box"
+													style={{
+														width: '180px',
+														height: '30px',
+													}}
+												/>
+											</Link>
+
+											<div
+												className="skeleton-box"
+												style={{
+													width: '100px',
+													height: '20px',
+												}}
 											/>
-										</Link>
-									</div>
 
-									<div className="nft__item_info">
-										<Link
-											to={`/item-details/${item.nftId}`}
-										>
-											<h4>{item.title}</h4>
-										</Link>
-
-										<div className="nft__item_price">
-											{item.price} ETH
-										</div>
-
-										<div className="nft__item_like">
-											<i className="fa fa-heart"></i>
-											<span>{item.likes}</span>
+											<div className="nft__item_like">
+												<div
+													className="skeleton-box"
+													style={{
+														width: '30px',
+														height: '15px',
+													}}
+												/>
+											</div>
 										</div>
 									</div>
 								</div>
-							</div>
-						))}
-					</OwlCarousel>
+							))}
+						</OwlCarousel>
+					) : (
+						<OwlCarousel
+							className="owl-theme"
+							{...options}
+							key={isLoading ? 'loading' : 'loaded'}
+						>
+							{newItemsData.map((item, index) => (
+								<div key={index}>
+									<div className="nft__item">
+										<div className="author_list_pp">
+											<Link
+												to={`/author/${item.authorId}`}
+												data-bs-toggle="tooltip"
+												data-bs-placement="top"
+												title="Creator: Monica Lucas"
+											>
+												<img
+													className="lazy"
+													src={item.authorImage}
+													alt=""
+												/>
+												<i className="fa fa-check"></i>
+											</Link>
+										</div>
+
+										{item.expiryDate && (
+											<CountDown
+												expireTime={item.expiryDate}
+											/>
+										)}
+
+										<div className="nft__item_wrap">
+											<div className="nft__item_extra">
+												<div className="nft__item_buttons">
+													<button>Buy Now</button>
+
+													<div className="nft__item_share">
+														<h4>Share</h4>
+
+														<a
+															href=""
+															target="_blank"
+															rel="noreferrer"
+														>
+															<i className="fa fa-facebook fa-lg"></i>
+														</a>
+
+														<a
+															href=""
+															target="_blank"
+															rel="noreferrer"
+														>
+															<i className="fa fa-twitter fa-lg"></i>
+														</a>
+
+														<a href="">
+															<i className="fa fa-envelope fa-lg"></i>
+														</a>
+													</div>
+												</div>
+											</div>
+
+											<Link
+												to={`/item-details/${item.nftId}`}
+											>
+												<img
+													src={item.nftImage}
+													className="lazy nft__item_preview"
+													alt=""
+												/>
+											</Link>
+										</div>
+
+										<div className="nft__item_info">
+											<Link
+												to={`/item-details/${item.nftId}`}
+											>
+												<h4>{item.title}</h4>
+											</Link>
+
+											<div className="nft__item_price">
+												{item.price} ETH
+											</div>
+
+											<div className="nft__item_like">
+												<i className="fa fa-heart"></i>
+												<span>{item.likes}</span>
+											</div>
+										</div>
+									</div>
+								</div>
+							))}
+						</OwlCarousel>
+					)}
 				</div>
 			</div>
 		</section>
