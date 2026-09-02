c4d2af25c1a8985db08448a722033387a91f04d3
Bryan <bdegante0906@gmail.com>
2026-09-02 16:03:45 -0500

    topsellers started

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 
 const TopSellers = () => {
+  useEffect(() => {
+    
+  })
   return (
     <section id="section-popular" className="pb-5">
       <div className="container">
